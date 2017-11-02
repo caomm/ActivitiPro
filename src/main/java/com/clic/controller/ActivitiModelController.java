a4abda3fa17819c84c4d58e3f658030dca864650
caomm <cao_manman@126.com>
2017-11-02 14:04:02 +0800

    修改内容

---
+++ src/main/java/com/clic/controller/ActivitiModelController.java
@@ -0,0 +1,166 @@
+package com.clic.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.vaadin.terminal.StreamResource;
+import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.engine.*;
+import org.activiti.engine.repository.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Created by DBQ on 2017/1/16.
+ */
+//@Component
+@Controller
+@RequestMapping("/model")
+public class ActivitiModelController {
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ActivitiModelController.class);
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
+    HistoryService historyService;
+
+    @Autowired
+    FormService formService;
+    @Autowired
+    private RepositoryService repositoryService;
+
+   /* @Autowired
+    private ObjectMapper objectMapper;*/
+
+    /*public void test(){
+        runtimeService.createProcessInstanceQuery().processDefinitionId("").list();
+        historyService.createHistoricTaskInstanceQuery().taskAssignee("").finished().list();
+
+
+        String parentTaskId = taskService.createTaskQuery().singleResult().getParentTaskId();
+        formService.getTaskFormData(parentTaskId).getFormProperties();
+
+        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processDefinitionId("").singleResult();
+
+
+        taskService.createTaskQuery().singleResult().getCreateTime();
+
+
+        historyService.createHistoricProcessInstanceQuery().unfinished().singleResult().getDurationInMillis();
+
+
+    }*/
+    /**
+     * 创建模型
+     */
+    @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public void create(@RequestParam("name") String name, @RequestParam("key") String key,
+                       @RequestParam(value = "description", required = false) String description, HttpServletRequest request,
+                       HttpServletResponse response) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode modelObjectNode = objectMapper.createObjectNode();
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, name);
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION,
+            org.apache.commons.lang3.StringUtils.defaultString(description));
+            Model newModel = repositoryService.newModel();
+            newModel.setMetaInfo(modelObjectNode.toString());
+            newModel.setName(name);
+            newModel.setKey(key);
+            repositoryService.saveModel(newModel);
+            ObjectNode editorNode = objectMapper.createObjectNode();
+            editorNode.put("id", "canvas");
+            editorNode.put("resourceId", "canvas");
+            ObjectNode stencilSetNode = objectMapper.createObjectNode();
+            stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
+            editorNode.put("stencilset", stencilSetNode);
+            repositoryService.addModelEditorSource(newModel.getId(), editorNode.toString().getBytes("utf-8"));
+            System.out.println(newModel.getId() + "===========newModel.getId()==============");
+            System.out.println("url:"+request.getContextPath() + "/modeler.html?modelId=" + newModel.getId());
+            response.sendRedirect(request.getContextPath()+"/modeler.html?modelId=" + newModel.getId());
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+
+    /**
+     * 模型列表
+     */
+    @RequestMapping("/list")
+    @ResponseBody
+    public Object list() {
+        List<Model> list = repositoryService.createModelQuery().list();
+        return list;
+    }
+
+    /**
+     * 获取流程图
+     *
+     * @throws IOException
+     */
+    @RequestMapping("/getImage")
+    public void getImage(String modelId, HttpServletResponse response) throws IOException {
+        StreamResource.StreamSource streamSource = null;
+        final byte[] editorSourceExtra = repositoryService.getModelEditorSourceExtra(modelId);
+        // final byte[] editorSource =
+        // repositoryService.getModelEditorSource(modelId);
+        if (editorSourceExtra != null) {
+            streamSource = new StreamResource.StreamSource() {
+                private static final long serialVersionUID = 1L;
+
+                public InputStream getStream() {
+                    InputStream inStream = null;
+                    try {
+                        inStream = new ByteArrayInputStream(editorSourceExtra);
+                    } catch (Exception e) {
+                        LOGGER.warn("Error reading PNG in StreamSource", e);
+                    }
+                    return inStream;
+                }
+            };
+        }
+        if (streamSource != null) {
+            response.setContentType("application/x-msdownload;charset=UTF-8");
+            response.reset();// 清除缓冲中的数据
+            int temp;
+            OutputStream os = null;
+            InputStream is = streamSource.getStream();
+            try {
+                os = response.getOutputStream();
+                while ((temp = is.read()) != (-1)) {
+                    os.write(temp);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                    os.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,13 +5,104 @@
   Time: 19:41
   To change this template use File | Settings | File Templates.
 --%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="st" uri="http://www.springframework.org/tags" %>
 <html>
   <head>
     <title>$Title$</title>
     <script type="text/javascript" src="jQuery/jquery-3.1.1.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
   </head>
   <body>
-  <a href="modeler.html?modelId=1547">编辑1547号流程</a>
+  <div id="test">
+    <a href="modeler.html?modelId=b92d430b-beec-11e7-8055-34f39a4ae408">编辑1号流程</a>
+    <a id="add" <%--href="/model/create?name='test002'&key='002'&description='002'"--%> >创建一个流程</a>
+
+  </div>
+
+
+  <form class="form-horizontal" id="myForm" action="<st:url value='/model/create'></st:url>"
+              method="post">
+            <div class="form-group">
+              <label for="name" class="col-sm-2 control-label">模型名字</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="name" name="name"
+                       placeholder="name">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="key" class="col-sm-2 control-label">模型key</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="key" name="key"
+                       placeholder="key">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="description" class="col-sm-2 control-label">模型名字</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="description"
+                       name="description" placeholder="模型描述">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+              <button type="submit" class="btn btn-primary" >提交</button>
+            </div>
+        </form>
+
+  <div class="row">
+    <div class="col-md-3" id="modelroot"></div>
+    <div class="col-md-9" id="image"></div>
+  </div>
+
   </body>
+
+  <script>
+    $("#add").click(function () {
+        alert("add")
+        /*$.ajax({
+            type:'post',
+            url:window.location.href +'/model/create',
+            data:{name:'test001',key:'123',description:'测试添加模板'},
+            async:false,
+            success:function () {
+                alert("添加成功");
+            },
+            error:function () {
+                alert("添加失败");
+            }
+        })*/
+
+        //$("#addmodel").modal("show");
+
+    });
+    var ctx = null;
+$(function () {
+    ctx='<%=request.getContextPath()%>';
+    getAllModel();
+
+})
+    function getAllModel() {
+        $.get(ctx+"/model/list",null,function(data){
+            $("#modelroot").empty();
+            $("#image").empty();
+            console.log(data);
+            var str='<div class="row" style="font-size: 20px;">所有流程模型</div>';
+            data.map((item)=>{
+                str+="<div class='row'><a href='#' onclick=\"getOne('"+item.id+"')\">"+item.name+"</a></div>";
+        });
+            $("#modelroot").append(str);
+        })
+    }
+
+    function getOne(id){
+        modelId=id;
+        //$.get("/model/getImage?modelId="+id,null,function(data){
+        $("#image").empty();
+        var str="<img src="+ctx+"/model/getImage?modelId="+id+" ></img>";
+        $("#image").append(str);
+        //})
+    }
+  </script>
 </html>
